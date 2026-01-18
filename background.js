c27bf90c56f958954a08f46b41adceb085400c9e
Miniblox697 <blockcraftia.official@gmail.com>
2026-01-17 22:35:16 -0600

    background.js

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.commands.onCommand.addListener(async (command) => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab?.id) return;
+  chrome.tabs.sendMessage(tab.id, { type: "YTB_COMMAND", command });
+});
